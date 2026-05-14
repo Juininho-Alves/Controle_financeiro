315b982c4d9e30d313ba045adede6173de125977
Juininho-Alves <juininhoalves11@gmail.com>
2026-05-13 22:52:08 -0300

    att

--- controller.py
+++ controller.py
@@ -1,19 +1,38 @@
 from flask import render_template, request, redirect, url_for
 from app import app
 from conexao import conexao
+from datetime import datetime
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def Index():
+    atual = datetime.now().strftime('%d/%m/%Y')
     conn = conexao()
     cursor = conn.cursor()
+    total_lucro = cursor.execute('SELECT SUM(valor) FROM movimentacoes WHERE tipo = "lucro"').fetchone()
+    dados = cursor.execute('SELECT * FROM movimentacoes').fetchall()
+
+    msg = None
 
     meta = cursor.execute(
         'SELECT COALESCE(valor_meta, 0) FROM meta').fetchone()
-    lucro = cursor.execute(
-        'SELECT COALESCE(SUM(valor_lucro),0) FROM lucro').fetchone()
 
-    return render_template('index.html', meta=f'{meta[0]:.2f}', lucro=f'{lucro[0]:.2f}')
+    if request.method == 'POST':
+        tipo_movimentacoes = request.form.get('tipo_movimentacoes')
+        motivo = request.form.get('motivo')
+        valor = request.form.get('valor')
+
+        if tipo_movimentacoes == None:
+            msg = 'Selecione o tipo de movimentação'
+
+        elif all([tipo_movimentacoes, motivo, valor]):
+            cursor.execute('INSERT INTO movimentacoes (tipo, motivo, dia, valor) VALUES (?,?,?,?)',
+                           (tipo_movimentacoes, motivo, atual, valor))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('Index'))
+
+    return render_template('index.html', meta=f'{meta[0]:.2f}', msg=msg, dados=dados,total_lucro=f'{total_lucro[0]:.2f}')
 
 
 @app.route('/cadastro', methods=['GET', 'POST'])
--- schemas.sql
+++ schemas.sql
@@ -2,19 +2,11 @@
 -- Criar uma tabela para os valores ganho no dia
 -- Criar uma tabela para gastos
 
-CREATE TABLE meta (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    valor_meta DECIMAL
-);
 
-CREATE TABLE lucro (
+CREATE TABLE movimentacoes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    valor_lucro DECIMAL,
-    dia TEXT
-);
-
-CREATE TABLE despesas (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    valor_despesas DECIMAL,
-    nome_despesa TEXT
+    tipo TEXT,
+    motivo TEXT,
+    dia TEXT,
+    valor DECIMAL
 );
